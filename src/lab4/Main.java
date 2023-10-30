bcae81cff94c54e110ec0e85b3cba05319da8a16
Gardos I.V. Sebi-Vasile <cti22b101@cst.uoradea.local>
2023-10-30 20:37:47 +0200

    Initial commit

---
+++ src/lab4/Main.java
@@ -0,0 +1,40 @@
+class Dog {
+    private String name;
+    private String breed;
+    public Dog(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getBreed() {
+        return breed;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("rex", "Labrador");
+        Dog dog2 = new Dog("john", "German Shepherd");
+
+        System.out.println("Initial Values:");
+        System.out.println("Dog 1 - Name: " + dog1.getName() + ", Breed: " + dog1.getBreed());
+        System.out.println("Dog 2 - Name: " + dog2.getName() + ", Breed: " + dog2.getBreed());
+
+        dog1.setName("Charlie");
+        dog1.setBreed("Golden Retriever");
+
+        dog2.setName("Rocky");
+        dog2.setBreed("Bulldog");
+
+        System.out.println("\nUpdated Values:");
+        System.out.println("Dog 1 - Name: " + dog1.getName() + ", Breed: " + dog1.getBreed());
+        System.out.println("Dog 2 - Name: " + dog2.getName() + ", Breed: " + dog2.getBreed());
+    }
+}
